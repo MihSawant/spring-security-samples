b35db22a76da2316378e96f8c1614677293dcbcc
Mihir Sawant <mihirssawant06@gmail.com>
2023-02-03 23:48:03 +0530

    initial commit

---
+++ src/test/java/sawant/mihir/springsecuritysamples/SpringSecuritySamplesApplicationTests.java
@@ -0,0 +1,13 @@
+package sawant.mihir.springsecuritysamples;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecuritySamplesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
